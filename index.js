a55219ddf504fede28dc614cd77ec4675d2eef25
muhammadsarimwaseem <sarimwaseem84@gmail.com>
2024-09-18 19:52:55 +0500

    updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8000;
 
-app.use(cors());
+app.use(cors({origin:"http://localhost:3000"}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: 'false' }));
 app.use(bodyParser.json());
